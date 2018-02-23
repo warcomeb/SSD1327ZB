2666a4378c4553ec99c9f3e7e8e020cd065f6404
Marco Giammarini <m.giammarini@warcomeb.it>
2018-02-23 18:19:45 +0100

    Added files

---
+++ ssd1327zb.c
@@ -0,0 +1,305 @@
+/******************************************************************************
+ * SSD1327ZB - Library for SSD1327ZB OLed Driver based on libohiboard
+ * Copyright (C) 2018 Alessio Socci & Marco Giammarini
+ *
+ * Authors:
+ *  Alessio Socci <alessios1284@hotmail.it>
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ ******************************************************************************/
+
+#include "ssd1327zb.h"
+
+#define SSD1327ZB_CMD_SETCOLUMNADDR              0x15
+#define SSD1327ZB_CMD_SETROWADDR                 0x75
+#define SSD1327ZB_CMD_SETCONTRAST                0x81
+#define SSD1327ZB_CMD_SEGMENTREMAP               0xA0 /**< Set Display remap */
+#define SSD1327ZB_CMD_STARTLINE                  0xA1 /**< Set start line */
+#define SSD1327ZB_CMD_DISPLAYNORMAL              0xA4
+#define SSD1327ZB_CMD_DISPLAYALLON               0xA5 /**< Entire Display is ON with gray scale level to GS15 */
+#define SSD1327ZB_CMD_DISPLAYALLOFF              0xA6 /**< Entire Display is OFF with gray scale level to GS0 */
+#define SSD1327ZB_CMD_DISPLAYINVERSE             0xA7
+#define SSD1327ZB_CMD_DISPLAYOFF                 0xAE
+#define SSD1327ZB_CMD_DISPLAYON                  0xAF
+
+#define SSD1327ZB_REMAP_COLUMN                   0x01 /**< Enable column address remap */
+#define SSD1327ZB_REMAP_NIBBLE                   0x02 /**< Enable nibble remap */
+#define SSD1327ZB_REMAP_ADDR_INCREMENT           0x04 /**< Enable vertical address increment */
+#define SSD1327ZB_REMAP_COM                      0x10 /**< Enable COM remap */
+#define SSD1327ZB_REMAP_ODDEVEN_COM              0x40 /**< Enable COM split odd/even */
+
+static void SSD1327ZB_write8 (SSD1327ZB_Device* dev, uint8_t value)
+{
+    ((value & 0x01) > 0) ? Gpio_set(dev->gdl.d0) : Gpio_clear(dev->gdl.d0);
+    ((value & 0x02) > 0) ? Gpio_set(dev->gdl.d1) : Gpio_clear(dev->gdl.d1);
+    ((value & 0x04) > 0) ? Gpio_set(dev->gdl.d2) : Gpio_clear(dev->gdl.d2);
+    ((value & 0x08) > 0) ? Gpio_set(dev->gdl.d3) : Gpio_clear(dev->gdl.d3);
+    ((value & 0x10) > 0) ? Gpio_set(dev->gdl.d4) : Gpio_clear(dev->gdl.d4);
+    ((value & 0x20) > 0) ? Gpio_set(dev->gdl.d5) : Gpio_clear(dev->gdl.d5);
+    ((value & 0x40) > 0) ? Gpio_set(dev->gdl.d6) : Gpio_clear(dev->gdl.d6);
+    ((value & 0x80) > 0) ? Gpio_set(dev->gdl.d7) : Gpio_clear(dev->gdl.d7);
+}
+
+static void SSD1327ZB_sendCommand (SSD1327ZB_Device* dev, uint8_t command)
+{
+#if defined WARCOMEB_GDL_PARALLEL
+
+    Gpio_set(dev->gdl.rd);
+    Gpio_clear(dev->gdl.cs);
+    Gpio_set(dev->gdl.wr);
+    // Is command message
+    Gpio_clear(dev->gdl.dc);
+    // Enable writing
+    Gpio_clear(dev->gdl.wr);
+    SSD1327ZB_write8(dev,command);
+    // Restore write pin
+    Gpio_set(dev->gdl.wr);
+    // Disable device
+    Gpio_set(dev->gdl.cs);
+
+#elif defined WARCOMEB_GDL_I2C
+
+#elif defined WARCOMEB_GDL_SPI
+
+#endif
+}
+
+static void SSD1327ZB_sendData (SSD1327ZB_Device* dev, uint8_t value)
+{
+#if defined WARCOMEB_GDL_PARALLEL
+
+    Gpio_set(dev->gdl.rd);
+    Gpio_clear(dev->gdl.cs);
+    Gpio_set(dev->gdl.wr);
+    // is data message
+    Gpio_set(dev->gdl.dc);
+    // Enable writing
+    Gpio_clear(dev->gdl.wr);
+    SSD1327ZB_write8(dev,value);
+    // Restore write pin
+    Gpio_set(dev->gdl.wr);
+    // Disable device
+    Gpio_set(dev->gdl.cs);
+
+#elif defined WARCOMEB_GDL_I2C
+
+#elif defined WARCOMEB_GDL_SPI
+
+#endif
+}
+
+/**
+ * The function set the current position into the display. The values are related to
+ * the internal buffer and not to the pixel of the display.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xPos The x position into the buffer
+ * @param[in] yPos The y position into the buffer
+ * @return Return an error if the requested position is wrong,
+ *         GDL_ERRORS_OK otherwise.
+ */
+static GDL_Errors SSD1327ZB_setBufferPosition (SSD1327ZB_Device* dev,
+                                               uint8_t xPos,
+                                               uint8_t yPos)
+{
+    if ((xPos >= dev->gdl.width) || (yPos >= dev->gdl.height))
+        return GDL_ERRORS_WRONG_POSITION;
+
+    // Set column address
+    SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_SETCOLUMNADDR);
+    SSD1327ZB_sendCommand(dev,xPos);
+    SSD1327ZB_sendCommand(dev,0x3F); // Max value!
+
+    // Set row address
+    SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_SETROWADDR);
+    SSD1327ZB_sendCommand(dev,yPos);
+    SSD1327ZB_sendCommand(dev,0x7F); // Max value!
+
+    return GDL_ERRORS_OK;
+}
+
+void SSD1327ZB_init (SSD1327ZB_Device* dev)
+{
+    // Set the device model
+    dev->gdl.model = GDL_MODELTYPE_SSD1327ZB;
+
+    // Save display size
+    dev->gdl.height = WARCOMEB_SSD1327ZB_HEIGHT;
+    dev->gdl.width = WARCOMEB_SSD1327ZB_WIDTH;
+
+    // Save default font size
+    dev->gdl.fontSize = 1;
+    dev->gdl.useCustomFont = FALSE;
+
+    // Save callback for drawing pixel
+    dev->gdl.drawPixel = SSD1327ZB_drawPixel;
+
+    memset(dev->buffer, 0x00, WARCOMEB_SSD1327ZB_BUFFERDIMENSION);
+
+#if defined WARCOMEB_GDL_PARALLEL
+
+    Gpio_config(dev->gdl.d0,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d1,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d2,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d3,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d4,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d5,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d6,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.d7,GPIO_PINS_OUTPUT);
+
+    Gpio_config(dev->gdl.rd,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.dc,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.rs,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.cs,GPIO_PINS_OUTPUT);
+    Gpio_config(dev->gdl.wr,GPIO_PINS_OUTPUT);
+
+    Gpio_set(dev->gdl.dc);
+    Gpio_set(dev->gdl.rd);
+    Gpio_set(dev->gdl.wr);
+    Gpio_set(dev->gdl.cs);
+    Gpio_set(dev->gdl.rs);
+
+    // Reset sequence
+    Gpio_set(dev->rstPin);
+    dev->gdl.delayTime(1);
+    Gpio_clear(dev->rstPin);
+    dev->gdl.delayTime(10);
+    Gpio_set(dev->rstPin);
+
+#elif defined WARCOMEB_GDL_I2C
+
+#elif defined WARCOMEB_GDL_SPI
+
+#endif
+
+    SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_DISPLAYON);
+    // Wait 100ms after on
+    dev->gdl.delayTime(100);
+
+    // Set display to normal
+    SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_DISPLAYNORMAL);
+
+    // Select segment re-map, COM scan direction, COM hardware configuration and
+    // de-select level
+    // They depend on producer choice
+    switch (dev->gdl.product)
+    {
+    case SSD1327ZB_PRODUCT_RAYSTAR_REX128128B:
+        SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_SEGMENTREMAP);
+        SSD1327ZB_sendCommand(dev,SSD1327ZB_REMAP_ODDEVEN_COM);
+
+        SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_STARTLINE);
+        SSD1327ZB_sendCommand(dev,0);
+        break;
+    }
+}
+
+GDL_Errors SSD1327ZB_drawPixel (SSD1327ZB_Device* dev,
+                                uint8_t xPos,
+                                uint8_t yPos,
+							    SSD1327ZB_GrayScale color)
+{
+    if ((xPos >= dev->gdl.width) || (yPos >= dev->gdl.height))
+        return GDL_ERRORS_WRONG_POSITION;
+
+    uint16_t pos = ((uint16_t) xPos/2) + ((uint16_t) (yPos/2)*(dev->gdl.width/2));
+
+    if (xPos%2)
+        dev->buffer[pos] = ((color & 0x0F) | (dev->dump[pos] & 0xF0));
+    else
+        dev->buffer[pos] = (((color << 4) & 0xF0) | (dev->buffer[pos] & 0x0F));
+
+    return GDL_ERRORS_OK;
+}
+
+void SSD1327ZB_setContrast (SSD1327ZB_Device* dev, uint8_t value)
+{
+    SSD1327ZB_sendCommand(dev,SSD1327ZB_CMD_SETCONTRAST);
+    SSD1327ZB_sendCommand(dev,value);
+}
+
+void SSD1327ZB_flush (SSD1327ZB_Device* dev)
+{
+    // Set the cursor to the starting point
+    SSD1327ZB_setBufferPosition(dev,0,0);
+
+    for (uint16_t i = 0; i < WARCOMEB_SSD1327ZB_BUFFERDIMENSION; i++)
+    {
+        SSD1327ZB_sendData(dev,dev->buffer[i]);
+    }
+}
+
+void SSD1327ZB_clear (SSD1327ZB_Device* dev)
+{
+    // Reset memory buffer
+    memset(dev->buffer, 0x00, WARCOMEB_SSD1327ZB_BUFFERDIMENSION);
+    // Flush the new buffer
+    SSD1327ZB_flush(dev);
+}
+
+void SSD1327ZB_drawLine (SSD1327ZB_Device* dev,
+                         uint8_t xStart,
+                         uint8_t yStart,
+                         uint8_t xStop,
+                         uint8_t yStop,
+                         SSD1327ZB_GrayScale color)
+{
+    GDL_drawLine(&(dev->gdl),xStart,yStart,xStop,yStop,(uint8_t)color);
+}
+
+void SSD1327ZB_drawHLine (SSD1327ZB_Device* dev,
+                          uint8_t xStart,
+                          uint8_t yStart,
+                          uint8_t width,
+                          SSD1327ZB_GrayScale color)
+{
+    SSD1327ZB_drawLine(dev,xStart,yStart,xStart+width,yStart,color);
+}
+
+void SSD1327ZB_drawVLine (SSD1306_Device* dev,
+                          uint8_t xStart,
+                          uint8_t yStart,
+                          uint8_t height,
+                          SSD1327ZB_GrayScale color)
+{
+    SSD1327ZB_drawLine(dev,xStart,yStart,xStart,yStart+height,color);
+}
+
+void SSD1327ZB_drawRectangle (SSD1327ZB_Device* dev,
+                              uint16_t xStart,
+                              uint16_t yStart,
+                              uint16_t width,
+                              uint16_t height,
+                              SSD1327ZB_GrayScale color,
+                              bool isFill)
+{
+    GDL_drawRectangle(&(dev->gdl),xStart,yStart,width,height,(uint8_t)color,isFill);
+}
+
+GDL_Errors SSD1327ZB_drawChar (SSD1327ZB_Device* dev,
+                               uint16_t xPos,
+                               uint16_t yPos,
+                               uint8_t c,
+                               SSD1327ZB_GrayScale color,
+                               uint8_t size)
+{
+    GDL_drawChar(&(dev->gdl),xPos,yPos,c,(uint8_t)color,0,size);
+}
---
+++ ssd1327zb.h
@@ -0,0 +1,258 @@
+/******************************************************************************
+ * SSD1327ZB - Library for SSD1327ZB OLed Driver based on libohiboard
+ * Copyright (C) 2018 Alessio Socci & Marco Giammarini
+ *
+ * Authors:
+ *  Alessio Socci <alessios1284@hotmail.it>
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ ******************************************************************************/
+
+#ifndef __WARCOMEB_SSD1327ZB_H
+#define __WARCOMEB_SSD1327ZB_H
+
+#define WARCOMEB_SSD1327ZB_LIBRARY_VERSION     "1.0.0"
+#define WARCOMEB_SSD1327ZB_LIBRARY_VERSION_M   1
+#define WARCOMEB_SSD1327ZB_LIBRARY_VERSION_m   0
+#define WARCOMEB_SSD1327ZB_LIBRARY_VERSION_bug 0
+#define WARCOMEB_SSD1327ZB_LIBRARY_TIME        0
+
+#include "libohiboard.h"
+#include "GDL/gdl.h"
+
+/*
+ * The user must define these label...
+ * The name of the define is:
+ *     #define WARCOMEB_SSD1327ZB_HEIGHT         xx
+ *     #define WARCOMEB_SSD1327ZB_WIDTH          xx
+ */
+#ifndef __NO_BOARD_H
+#include "board.h"
+#endif
+
+#if !defined(WARCOMEB_SSD1327ZB_HEIGHT) | !defined(WARCOMEB_SSD1327ZB_WIDTH)
+#error "You must define height and width of display!"
+#else
+// Every byte represent 2 pixel...
+#define WARCOMEB_SSD1327ZB_BUFFERDIMENSION (WARCOMEB_SSD1327ZB_WIDTH*WARCOMEB_SSD1327ZB_HEIGHT/2)
+#endif
+
+#if defined(WARCOMEB_SSD1327ZB_HEIGHT) & ((WARCOMEB_SSD1327ZB_HEIGHT < 16) | (WARCOMEB_SSD1327ZB_HEIGHT > 128))
+#error "The height must be between 16 and 128!"
+#endif
+
+#if defined(WARCOMEB_SSD1327ZB_WIDTH) & ((WARCOMEB_SSD1327ZB_WIDTH < 16) | (WARCOMEB_SSD1327ZB_WIDTH > 128))
+#error "The width must be between 16 and 128!"
+#endif
+
+/**
+ * A usefull enum that define all the possbile color for each pixel.
+ */
+typedef enum _SSD1327ZB_GrayScale
+{
+	SSD1327ZB_GRAYSCALE_0  = 0,
+	SSD1327ZB_GRAYSCALE_1  = 1,
+	SSD1327ZB_GRAYSCALE_2  = 2,
+	SSD1327ZB_GRAYSCALE_3  = 3,
+	SSD1327ZB_GRAYSCALE_4  = 4,
+	SSD1327ZB_GRAYSCALE_5  = 5,
+	SSD1327ZB_GRAYSCALE_6  = 6,
+	SSD1327ZB_GRAYSCALE_7  = 7,
+	SSD1327ZB_GRAYSCALE_8  = 8,
+	SSD1327ZB_GRAYSCALE_9  = 9,
+	SSD1327ZB_GRAYSCALE_10 = 10,
+	SSD1327ZB_GRAYSCALE_11 = 11,
+	SSD1327ZB_GRAYSCALE_12 = 12,
+	SSD1327ZB_GRAYSCALE_13 = 13,
+	SSD1327ZB_GRAYSCALE_14 = 14,
+	SSD1327ZB_GRAYSCALE_15 = 15,
+} SSD1327ZB_GrayScale;
+
+typedef enum _SSD1327ZB_Product
+{
+	SSD1327ZB_PRODUCT_RAYSTAR_REX128128B   = 0x0001 | GDL_MODELTYPE_SSD1327ZB,
+} SSD1327ZB_Product;
+
+typedef struct SSD1327ZB_Device
+{
+    GDL_Device gdl;                         /**< Common part for each device */
+
+#if defined WARCOMEB_GDL_PARALLEL
+
+#elif defined WARCOMEB_GDL_I2C
+
+    Gpio_Pins rstPin;            /**< Reset pin used for start-up the display */
+
+#elif defined WARCOMEB_GDL_SPI
+
+#endif
+
+    /** Buffer to store display data */
+    uint8_t buffer [WARCOMEB_SSD1327ZB_BUFFERDIMENSION];
+
+} SSD1327ZB_Device;
+
+/**
+ *
+ */
+void SSD1327ZB_init (SSD1327ZB_Device* dev);
+
+/**
+ *
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xPos The x position
+ * @param[in] yPos The y position
+ * @param[in] color The color of the pixel
+ * @return
+ */
+GDL_Errors SSD1327ZB_drawPixel (SSD1327ZB_Device* dev,
+                                uint8_t xPos,
+                                uint8_t yPos,
+							    SSD1327ZB_GrayScale color);
+
+/**
+ * The function print a line in the selected position with the selected
+ * color.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xStart The starting x position
+ * @param[in] yStart The starting y position
+ * @param[in] xStop The ending x position
+ * @param[in] yStop The ending y position
+ * @param[in] color The color of the line
+ */
+void SSD1327ZB_drawLine (SSD1327ZB_Device* dev,
+                         uint8_t xStart,
+                         uint8_t yStart,
+                         uint8_t xStop,
+                         uint8_t yStop,
+					     SSD1327ZB_GrayScale color);
+
+/**
+ * The function print a horizontal line from the selected position with
+ * the selected width and color.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xStart The starting x position
+ * @param[in] yStart The starting y position
+ * @param[in] width The width of the line
+ * @param[in] color The color of the line
+ */
+void SSD1327ZB_drawHLine (SSD1327ZB_Device* dev,
+                          uint8_t xStart,
+                          uint8_t yStart,
+                          uint8_t width,
+                          SSD1327ZB_GrayScale color);
+
+/**
+ * The function print a vertical line from the selected position with
+ * the selected height and color.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xStart The starting x position
+ * @param[in] yStart The starting y position
+ * @param[in] height The height of the line
+ * @param[in] color The color of the line
+ */
+void SSD1327ZB_drawVLine (SSD1327ZB_Device* dev,
+                          uint8_t xStart,
+                          uint8_t yStart,
+                          uint8_t height,
+                          SSD1327ZB_GrayScale color);
+
+/**
+ * The function draw a rectangle. It can be fill or not.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xStart The starting x position
+ * @param[in] yStart The starting y position
+ * @param[in] width The ending x position
+ * @param[in] height The ending y position
+ * @param[in] color The color of the rectangle
+ * @param[in] isFill If TRUE the rectangle will be fill
+ */
+void SSD1327ZB_drawRectangle (SSD1327ZB_Device* dev,
+                              uint16_t xStart,
+                              uint16_t yStart,
+                              uint16_t width,
+                              uint16_t height,
+							  SSD1327ZB_GrayScale color,
+                              bool isFill);
+
+/**
+ * The function print a char in the selected position with the selected
+ * color and size.
+ * The starting point is the top-left corner of the char.
+ *
+ * @param[in] dev The handle of the device
+ * @param[in] xPos The x position
+ * @param[in] yPos The y position
+ * @param[in] c The char to be draw
+ * @param[in] color The foreground color of the char
+ * @param[in] size The size for the char, if 0 use default dimension
+ * @return GDL_ERRORS_WRONG_POSITION if the dimension plus position of the char
+ *         exceeds the width or height of the display, GDL_ERRORS_OK otherwise.
+ */
+GDL_Errors SSD1327ZB_drawChar (SSD1327ZB_Device* dev,
+                               uint16_t xPos,
+                               uint16_t yPos,
+                               uint8_t c,
+							   SSD1327ZB_GrayScale color,
+                               uint8_t size);
+
+
+/**
+ * @param[in] dev The handle of the device
+ * @param[in] value The 256 contrast steps from 00h to FFh
+ */
+void SSD1327ZB_setContrast (SSD1327ZB_Device* dev, uint8_t value);
+
+/**
+ *
+ *
+ * @param[in] dev The handle of the device
+ */
+//void SSD1306_inverseDisplay (SSD1327ZB_Device* dev);
+
+/**
+ *
+ *
+ * @param[in] dev The handle of the device
+ */
+//void SSD1306_normalDisplay (SSD1327ZB_Device* dev);
+
+/**
+ * This function clear the display setting off all pixel
+ *
+ * @param[in] dev The handle of the device
+ */
+void SSD1327ZB_clear (SSD1327ZB_Device* dev);
+
+/**
+ *
+ *
+ * @param[in] dev The handle of the device
+ */
+void SSD1327ZB_flush (SSD1327ZB_Device* dev);
+
+#endif /* __WARCOMEB_SSD1327ZB_H */
+
